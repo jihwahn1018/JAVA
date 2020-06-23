97c4617228eba742fb10cfa3d4f8f5ef1f70726c
jihwahn1018 <37329458+jihwahn1018@users.noreply.github.com>
2020-06-24 00:46:37 +0900

    Create TextAreaDemo.java

---
+++ basic/TextAreaDemo.java
@@ -0,0 +1,60 @@
+package test;
+
+
+import java.awt.*;
+import java.awt.event.*;
+
+ 
+
+public class TextAreaDemo{
+
+	private Frame f;
+	private TextArea tf;
+
+	
+
+	public TextAreaDemo() {
+
+		f= new Frame("TextArea");
+		tf= new TextArea("welcome to sogang!",5,30);
+		tf.addKeyListener(new NameHandler());
+		tf.addTextListener(new TextHandler());
+		f.add(tf,BorderLayout.CENTER);
+		f.pack();
+		WindowDestroyer listener = new WindowDestroyer();
+		f.addWindowListener(listener);
+		f.setVisible(true);
+
+	}
+
+	
+
+	class NameHandler extends KeyAdapter{
+
+		public void keyPressed(KeyEvent e) {
+			char c = e.getKeyChar();
+			if(Character.isDigit(c))
+				e.consume();
+		}
+
+		public void keyTyped(KeyEvent e) {}
+		public void keyReleased(KeyEvent e){}
+
+	}
+
+	class TextHandler implements TextListener{
+		public void textValueChanged (TextEvent e) {
+				System.out.println(tf.getText());
+
+		}
+
+	}
+
+	
+
+		public static void main(String args[]) {
+			TextAreaDemo tfd = new TextAreaDemo();
+		}
+
+	
+}

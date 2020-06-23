6d9e20ee7562fdab4000968f1aea6ec37d9ec6d1
jihwahn1018 <37329458+jihwahn1018@users.noreply.github.com>
2020-06-24 00:47:24 +0900

    Create windowDestroyer.java

---
+++ basic/windowDestroyer.java
@@ -0,0 +1,11 @@
+package test;
+
+import java.awt.*;
+import java.awt.event.*;
+
+public class WindowDestroyer extends WindowAdapter{
+	public void windowClosing(WindowEvent e)
+	{
+		System.exit(0);
+	}
+}
